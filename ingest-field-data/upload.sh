f4251cea0eb7489ccbb4222791cdb5254cf64953
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-12-07 12:33:04 -0800

    fix: creating uuid command line alias is not necessary

--- ingest-field-data/upload.sh
+++ ingest-field-data/upload.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-alias uuid="python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)' | pbcopy && pbpaste && echo"
-CAPTURE_UUID_VALUE=`uuid`
-REGISTRATION_UUID_VALUE=`uuid`
-sed "s/CAPTURE_UUID_VALUE/$CAPTURE_UUID_VALUE/" template/captures.json > prepared/captures.json
-sed "s/REGISTRATION_UUID_VALUE/$REGISTRATION_UUID_VALUE/" template/registrations.json > prepared/registrations.json
+set -e
+CAPTURE_UUID_VALUE=`python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)'`
+REGISTRATION_UUID_VALUE=`python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)'`
+sed "s/CAPTURE_UUID_VALUE/$CAPTURE_UUID_VALUE/" template/testing-tool-captures.json > prepared/testing-tool-captures.json
+sed "s/REGISTRATION_UUID_VALUE/$REGISTRATION_UUID_VALUE/" template/testing-tool-registrations.json > prepared/testing-tool-registrations.json
 
 #upload it
-aws s3 cp --profile treetracker-test-env prepared/captures.json s3://treetracker-test-batch-uploads
-aws s3 cp --profile treetracker-test-env prepared/registrations.json s3://treetracker-test-batch-uploads
+aws s3 cp --profile treetracker-$ENV-env prepared/testing-tool-captures.json s3://treetracker-$ENV-batch-uploads
+aws s3 cp --profile treetracker-$ENV-env prepared/testing-tool-registrations.json s3://treetracker-$ENV-batch-uploads
 
 echo "capture uuid: $CAPTURE_UUID_VALUE"
 echo "registration uuid: $REGISTRATION_UUID_VALUE"

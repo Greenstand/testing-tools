823f9d525a0f25b09590331dd02ea25e3fc4f5c5
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-01-20 13:13:10 -0800

    feat: use current timestamp on captures

--- ingest-field-data/upload.sh
+++ ingest-field-data/upload.sh
@@ -2,8 +2,10 @@
 set -e
 CAPTURE_UUID_VALUE=`python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)'`
 REGISTRATION_UUID_VALUE=`python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)'`
+EPOCH=`date +%s`
 sed "s/CAPTURE_UUID_VALUE/$CAPTURE_UUID_VALUE/" template/testing-tool-captures.json > prepared/testing-tool-captures.json
 sed "s/REGISTRATION_UUID_VALUE/$REGISTRATION_UUID_VALUE/" template/testing-tool-registrations.json > prepared/testing-tool-registrations.json
+sed -i .bck "s/CAPTURE_TIMESTAMP/$EPOCH/" prepared/testing-tool-captures.json
 
 #upload it
 aws s3 cp --profile treetracker-$ENV-env prepared/testing-tool-captures.json s3://treetracker-$ENV-batch-uploads

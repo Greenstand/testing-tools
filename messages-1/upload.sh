1beb88a1181fcb09a12d3f9702200e01722d4ec0
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-02-20 14:10:48 -0800

    feat: data creation tool for bulk pack v2

---
+++ messages-1/upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+MESSAGE_UUID_VALUE=`python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)'`
+EPOCH=`date +%s`
+rm prepared/*.json
+sed "s/MESSAGE_UUID_VALUE/$MESSAGE_UUID_VALUE/" template/survey-message.json > prepared/$EPOCH-testing-tool-survey-message.json
+
+MESSAGE_UUID_VALUE=`python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)'`
+sed "s/MESSAGE_UUID_VALUE/$MESSAGE_UUID_VALUE/" template/normal-message.json > prepared/$EPOCH-testing-tool-normal-message.json
+
+#upload it
+aws s3 cp --profile treetracker-$ENV-env prepared/$EPOCH-testing-tool-survey-message.json s3://treetracker-$ENV-batch-uploads
+aws s3 cp --profile treetracker-$ENV-env prepared/$EPOCH-testing-tool-normal-message.json s3://treetracker-$ENV-batch-uploads
+
+echo "message uuid: $MESSAGE_UUID_VALUE"
+
+sleep 3
+kubectl -n bulk-pack-services delete job bulk-pack-processor-manual-001
+kubectl -n bulk-pack-services create job --from=cronjob/bulk-pack-processor bulk-pack-processor-manual-001

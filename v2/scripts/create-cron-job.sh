57617dd33418ae7689fc44e21f220e9ba5b15037
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-02-22 15:04:17 -0800

    fix: use UUID for job name

--- v2/scripts/create-cron-job.sh
+++ v2/scripts/create-cron-job.sh
@@ -1,2 +1,2 @@
-kubectl -n bulk-pack-services delete job bulk-pack-processor-manual-001
-kubectl -n bulk-pack-services create job --from=cronjob/bulk-pack-processor bulk-pack-processor-manual-001
+UUID=$(python scripts/uuid4.py)
+kubectl -n bulk-pack-services create job --from=cronjob/bulk-pack-processor bulk-pack-processor-manual-$UUID

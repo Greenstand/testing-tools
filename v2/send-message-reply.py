84b48c6879dec504a7f4222f6c7c5f9e25f6e85b
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-03-01 15:27:29 -0800

    feat: send message reply script

---
+++ v2/send-message-reply.py
@@ -0,0 +1,72 @@
+import datetime
+import requests
+import sys
+import json
+
+api = 'https://dev-k8s.treetracker.org/messaging/'
+
+# -- get optional arguments --
+
+try:
+    recipientHandle = sys.argv[1]
+except:
+    recipientHandle = "handle2"
+
+try:
+    authorHandle = sys.argv[2]
+except:
+    authorHandle = "handle3"
+
+try:
+    messageBody = sys.argv[3]
+except:
+    messageBody = "testing-tools message body"
+
+
+# -- get latest message --
+
+getLatestMessageRoute = '{api}message?handle={author}&limit=1'.format(
+    api=api,
+    author=recipientHandle,
+)
+
+print("get message route:")
+print(getLatestMessageRoute)
+
+res = requests.get(getLatestMessageRoute)
+print("attempting to get latest message from author: ", recipientHandle)
+print("get message result: ", res.status_code)
+
+try:
+    latestMessage = res.json()['messages'][0]
+except:
+    print("could not find message, script is exiting")
+    sys.exit()
+
+print("message that will be replied to:")
+print(json.dumps(latestMessage, indent=2, sort_keys=True))
+
+
+# generate reply
+surveyId = latestMessage['survey']['id']
+
+message = {}
+message["survey_id"] = surveyId
+message["type"] = "message"
+message["recipient_handle"] = recipientHandle
+message["author_handle"] = authorHandle
+message["subject"] = "message subject"
+message["body"] = messageBody
+
+print("sending this message:")
+print(json.dumps(message, indent=2, sort_keys=True))
+
+# send reply
+
+postMessageRoute = '{api}message'.format(api=api)
+
+print("posting message route:")
+print(postMessageRoute)
+
+postRes = requests.post(postMessageRoute, json=message)
+print("post message result: ", postRes.status_code)
